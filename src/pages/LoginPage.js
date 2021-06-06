b05726d9a6d1413de44c3b2d5dbeecf33c169309
BigunetsT <tanyuschamail83@gmail.com>
2021-06-06 10:50:46 +0300

    use hooks

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { authOperations } from '../redux/auth';
 import {
   FormControl,
@@ -14,89 +14,77 @@ import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import '../styles.scss';
 
-class LoginPage extends Component {
-  static propTypes = {
-    state: PropTypes.shape({
-      email: PropTypes.string.isRequired,
-      password: PropTypes.string.isRequired,
-      showPassword: PropTypes.bool,
-    }),
-    onSubmit: PropTypes.func,
-  };
-  state = {
+export default function LoginPage() {
+  const [formData, setFormData] = useState({
     email: '',
     password: '',
-    showPassword: false,
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
   };
-  handleClickShowPassword = () => {
-    this.setState({ ...this.state, showPassword: !this.state.showPassword });
-  };
-
-  handleMouseDownPassword = event => {
+  const handleMouseDownPassword = event => {
     event.preventDefault();
   };
-
-  handleChange = prop => event => {
-    this.setState({ ...this.state, [prop]: event.target.value });
+  const handleChange = name => event => {
+    setFormData({
+      ...formData,
+      [name]: event.target.value,
+    });
   };
-
-  handleSubmit = e => {
+  const dispatch = useDispatch();
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onLogin(this.state);
-    this.setState({ email: '', password: '', showPassword: false });
+    dispatch(authOperations.logIn(formData));
+    setFormData({
+      email: '',
+      password: '',
+    });
   };
 
-  render() {
-    const { email, password, showPassword } = this.state;
-
-    return (
-      <div className="wrapper">
-        <h1 className="title">Сторінка входу</h1>
-        <form onSubmit={this.handleSubmit} className="form" autoComplete="on">
-          <TextField
-            id="outlined-basic"
-            label="Пошта"
-            variant="outlined"
-            value={email}
-            name="email"
-            onChange={this.handleChange('email')}
+  return (
+    <div className="wrapper">
+      <h1 className="title">Сторінка входу</h1>
+      <form onSubmit={handleSubmit} className="form" autoComplete="on">
+        <TextField
+          id="outlined-basic"
+          label="Пошта"
+          variant="outlined"
+          name="email"
+          onChange={handleChange('email')}
+        />
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Пароль</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            name="password"
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={70}
           />
-          <FormControl variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-password">
-              Пароль
-            </InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={showPassword ? 'text' : 'password'}
-              value={password}
-              name="password"
-              onChange={this.handleChange('password')}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={this.handleClickShowPassword}
-                    onMouseDown={this.handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              labelWidth={70}
-            />
-          </FormControl>
+        </FormControl>
 
-          <button type="submit" className="formBtn">
-            Ввійти
-          </button>
-        </form>
-      </div>
-    );
-  }
+        <button type="submit" className="formBtn">
+          Ввійти
+        </button>
+      </form>
+    </div>
+  );
 }
-const mapDispatchToProps = {
-  onLogin: authOperations.logIn,
+LoginPage.propTypes = {
+  email: PropTypes.string,
+  password: PropTypes.string,
+  showPassword: PropTypes.bool,
 };
-
-export default connect(null, mapDispatchToProps)(LoginPage);
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { authOperations } from '../redux/auth';
 import '../styles.scss';
 import {
@@ -14,113 +14,91 @@ import {
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
-class RegisterPage extends Component {
-  static propTypes = {
-    state: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      email: PropTypes.string.isRequired,
-      password: PropTypes.string.isRequired,
-    }),
-    onSubmit: PropTypes.func,
-  };
-  state = {
+export default function RegisterPage() {
+  const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: '',
+  });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
   };
 
-  static propTypes = {
-    state: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      email: PropTypes.string.isRequired,
-      password: PropTypes.string.isRequired,
-      showPassword: PropTypes.bool,
-    }),
-    onSubmit: PropTypes.func,
-  };
-  state = {
-    name: '',
-    email: '',
-    password: '',
-    showPassword: false,
-  };
-  handleClickShowPassword = () => {
-    this.setState({ ...this.state, showPassword: !this.state.showPassword });
-  };
-
-  handleMouseDownPassword = event => {
+  const handleMouseDownPassword = event => {
     event.preventDefault();
   };
 
-  handleChange = prop => event => {
-    this.setState({ ...this.state, [prop]: event.target.value });
+  const handleChange = name => event => {
+    setFormData({
+      ...formData,
+      [name]: event.target.value,
+    });
   };
-
-  handleSubmit = e => {
+  const dispatch = useDispatch();
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onRegister(this.state);
-    this.setState({ name: '', email: '', password: '', showPassword: false });
+    dispatch(authOperations.register(formData));
+    setFormData({
+      name: '',
+      email: '',
+      password: '',
+    });
   };
 
-  render() {
-    const { name, email, password, showPassword } = this.state;
-
-    return (
-      <div className="wrapper">
-        <h1 className="title">Сторінка реєстрації</h1>
-        <form onSubmit={this.handleSubmit} className="form" autoComplete="on">
-          <TextField
-            id="outlined-basic"
-            label="Ім'я"
-            variant="outlined"
-            value={name}
-            name="name"
-            onChange={this.handleChange('name')}
+  return (
+    <div className="wrapper">
+      <h1 className="title">Сторінка реєстрації</h1>
+      <form onSubmit={handleSubmit} className="form" autoComplete="on">
+        <TextField
+          id="outlined-basic"
+          label="Ім'я"
+          variant="outlined"
+          name="name"
+          onChange={handleChange('name')}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Пошта"
+          variant="outlined"
+          name="email"
+          onChange={handleChange('email')}
+        />
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Пароль</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            name="password"
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={70}
           />
-          <TextField
-            id="outlined-basic"
-            label="Пошта"
-            variant="outlined"
-            value={email}
-            name="email"
-            onChange={this.handleChange('email')}
-          />
-          <FormControl variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-password">
-              Пароль
-            </InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={showPassword ? 'text' : 'password'}
-              value={password}
-              name="password"
-              onChange={this.handleChange('password')}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={this.handleClickShowPassword}
-                    onMouseDown={this.handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              labelWidth={70}
-            />
-          </FormControl>
+        </FormControl>
 
-          <button type="submit" className="formBtn">
-            Зареєструватися
-          </button>
-        </form>
-      </div>
-    );
-  }
+        <button type="submit" className="formBtn">
+          Зареєструватися
+        </button>
+      </form>
+    </div>
+  );
 }
-const mapDispatchToProps = {
-  onRegister: authOperations.register,
-};
 
-export default connect(null, mapDispatchToProps)(RegisterPage);
+RegisterPage.propTypes = {
+  name: PropTypes.string,
+  email: PropTypes.string,
+  password: PropTypes.string,
+  showPassword: PropTypes.bool,
+};
